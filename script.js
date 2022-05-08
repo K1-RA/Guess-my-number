b33a38ab7a04334a7f20fcff1217cdde71e3d4c5
K1-RA <indrajeetgodse@gmail.com>
2022-05-08 22:46:50 +0200

    updates
    JS

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-'use strict';
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+let displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(typeof guess, guess);
+
+  // COMMENT when there is no input
+  if (!guess) {
+    // document.querySelector(".message").textContent = "☢️ No number entered"
+    displayMessage("☢️ No number entered");
+
+    // COMMENT when the guess is correct
+  } else if (guess === secretNumber) {
+    displayMessage("FUCK YEAH!🍾");
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+
+    // COMMENT when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "Too high!🚬" : "Too low!💊");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💀 Better luck next time!");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage("Start guessing");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
+
+document.querySelector(".highscore").textContent = highscore;
